6eace83ce868d3cc20242985ee1cdb55585d9edb
catplane <nihaoyawoshixixida@gmail.com>
2022-01-17 23:57:18 +0800

    sqlalchemy train

---
+++ stitcmonster/models/user.py
@@ -0,0 +1,22 @@
+from operator import index
+from turtle import back
+from flask_login import UserMixin, AnonymousUserMixin
+from stitcmonster.corelib.exts import db
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relastionship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+
+
+class User(UserMixin, db.Model):
+    pass
